5ff85052980daedbcfe6f6677cc2ab530a45bfce
NaughtDZ <1603705420@qq.com>
2024-02-24 22:25:07 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label2 = New Label()
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        ToolTip1 = New ToolTip(components)
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(97, 26)
+        Button1.TabIndex = 0
+        Button1.Text = "图片文件夹"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(12, 44)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(97, 27)
+        Button2.TabIndex = 1
+        Button2.Text = "导出文件夹"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(12, 77)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(351, 27)
+        Button3.TabIndex = 2
+        Button3.Text = "运行"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 113)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(68, 17)
+        Label1.TabIndex = 3
+        Label1.Text = "最长边大小"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(115, 12)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(248, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(115, 48)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(248, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(86, 110)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(84, 23)
+        TextBox3.TabIndex = 6
+        TextBox3.Text = "768"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.ForeColor = Color.Gray
+        Label2.Location = New Point(242, 113)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(56, 17)
+        Label2.TabIndex = 7
+        Label2.Text = "准备就绪"
+        ' 
+        ' ToolTip1
+        ' 
+        ToolTip1.AutoPopDelay = 5000
+        ToolTip1.InitialDelay = 200
+        ToolTip1.ReshowDelay = 100
+        ToolTip1.ToolTipTitle = "注意检查文件夹内文件格式"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 17F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(371, 144)
+        Controls.Add(Label2)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "EzRescale"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents ToolTip1 As ToolTip
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports System.IO
+Imports System.Threading
+
+Public Class Form1
+    Private Sub ToolTip1_Popup(sender As Object, e As PopupEventArgs) Handles ToolTip1.Popup
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ToolTip1.SetToolTip(Button1, "确保格式为jpg,jpeg,png,bmp格式！")
+        Button3.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FolderBrowserDialog1.ShowDialog()
+        If IO.Directory.Exists(FolderBrowserDialog1.SelectedPath) = True Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+            Butt_enableCheck()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        FolderBrowserDialog1.ShowDialog()
+        If IO.Directory.Exists(FolderBrowserDialog1.SelectedPath) = True Then
+            TextBox2.Text = FolderBrowserDialog1.SelectedPath
+            Butt_enableCheck()
+        End If
+    End Sub
+
+    Private Sub Butt_enableCheck()
+        If IO.Directory.Exists(TextBox1.Text) And IO.Directory.Exists(TextBox2.Text) Then
+            Button3.Enabled = True
+            Label2.Text = "准备就绪"
+            Label2.ForeColor = Color.Gray
+        Else
+            Button3.Enabled = False
+            Label2.Text = "准备就绪"
+            Label2.ForeColor = Color.Gray
+        End If
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button3.Enabled = False
+        Label2.Text = "处理中"
+        Label2.ForeColor = Color.Green
+        Dim loadfiles As String()
+        ' 获取指定文件夹中所有文件的名称
+        Try
+            loadfiles = System.IO.Directory.GetFiles(TextBox1.Text)
+        Catch ex As Exception
+            MsgBox(ex)
+            Exit Sub
+        End Try
+        For Each img In loadfiles
+            Rescale(img, TextBox3.Text, TextBox2.Text & "\" & Path.GetFileName(img))
+            Application.DoEvents()
+        Next
+        Beep()
+        Label2.Text = "处理完成"
+        Label2.ForeColor = Color.BlueViolet
+        Button3.Enabled = True
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Butt_enableCheck()
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        Butt_enableCheck()
+    End Sub
+
+    Public Sub Rescale(ByVal imagePath As String, ByVal maxLength As Integer, ByVal savePath As String)
+
+        ' 读取图片
+        Dim image As Image = Image.FromFile(imagePath)
+
+        ' 识别图片方向
+        Dim isHorizontal As Boolean = image.Width > image.Height
+
+        ' 计算缩放比例
+        Dim scale As Double = 1.0
+        If isHorizontal Then
+            scale = maxLength / image.Width
+        Else
+            scale = maxLength / image.Height
+        End If
+
+        ' 等比例缩放图片
+        Dim newImage As Image = New Bitmap(CType(image.Width * scale, Integer), CType(image.Height * scale, Integer))
+        Using graphics As Graphics = Graphics.FromImage(newImage)
+            graphics.DrawImage(image, New Rectangle(0, 0, newImage.Width, newImage.Height), New Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel)
+        End Using
+
+        ' 另存为图片
+        newImage.Save(savePath)
+    End Sub
+
+
+
+End Class
